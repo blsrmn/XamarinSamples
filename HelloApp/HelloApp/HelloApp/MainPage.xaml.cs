56a338ce3ba60521cc6a7e0d27c40cd0c79a78a2
ikoroleva <prsml.ellisson@gmail.com>
2017-12-05 00:13:33 +0500

    init HjackApp

---
+++ HelloApp/HelloApp/HelloApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Button1.Clicked += delegate { this.Button1.Text = $"Хуяксов: {count++}"; };
+        }
+    }
+}
